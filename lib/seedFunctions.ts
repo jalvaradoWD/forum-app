ad268db5b7e1f87cf36e0ea550043dbaa5dc21c2
jalvaradoWD <josealvarado1337@gmail.com>
2022-06-18 13:14:38 -0500

    Trying to add additional seed functrions.

--- lib/seedFunctions.ts
+++ lib/seedFunctions.ts
@@ -1,5 +1,7 @@
+import { User, Topic } from './../node_modules/.prisma/client/index.d';
 import { faker } from '@faker-js/faker';
 import bcrypt from 'bcrypt';
+import { prisma } from './db';
 
 export const generateFakeUser = async (n: number) => {
   const result = [];
@@ -12,6 +14,40 @@ export const generateFakeUser = async (n: number) => {
     const hashedPassword = await bcrypt.hash('password', 10);
 
     result.push({ email, password: hashedPassword, name: fullName });
+  }
+
+  return result;
+};
+
+export const updateUsersTopics = async (
+  topics: Topic[],
+  user: User,
+  categoryId: string
+) => {
+  await prisma.user.update({
+    where: { id: user.id },
+    data: {
+      Topic: {
+        createMany: { data: await generateUserTopics(topics, categoryId) },
+      },
+    },
+  });
+};
+
+const generateUserTopics = async (
+  topics: Topic[],
+  categoryId: string
+): Promise<any[]> => {
+  let result: any[] = [];
+
+  Math.floor(Math.random() * topics.length);
+
+  for (let i = 0; i < Math.floor(Math.random() * 20); i++) {
+    result.push({
+      title: faker.lorem.words(Math.floor(Math.random() * 3) + 1),
+      description: faker.lorem.paragraphs(Math.floor(Math.random() * 2)),
+      categoryId,
+    });
   }
 
   return result;
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,16 +1,73 @@
 import { PrismaClient } from '@prisma/client';
-import { generateFakeUser } from '../lib/seedFunctions';
+import { generateFakeUser, updateUsersTopics } from '../lib/seedFunctions';
 
 const prismaClient = new PrismaClient();
 
 async function main() {
-  const generatedUsers = await generateFakeUser(100);
+  const generatedUsers = await generateFakeUser(20);
 
-  const addedUsers = await prismaClient.user.createMany({
+  await prismaClient.user.createMany({
     data: generatedUsers,
   });
 
-  console.log(`Generated ${addedUsers.count} users.`);
+  await prismaClient.forum.createMany({
+    data: [
+      {
+        name: 'S&box',
+        description:
+          'Based on the Source 2 engine and is currently in heavy development.',
+      },
+      {
+        name: 'Rust',
+        description: 'Help and discussion on our hugely popular survival game.',
+      },
+      {
+        name: "Garry's Mod",
+        description:
+          'Help and discussion surrounding our Source Engine 1 based sandbox game',
+      },
+    ],
+  });
+
+  const forums = await prismaClient.forum.findMany({});
+  const topics = await prismaClient.topic.findMany({});
+  const category = await prismaClient.category.findMany({});
+  const users = await prisma?.user.findMany({});
+
+  forums.forEach(async (forum) => {
+    await prismaClient?.category.createMany({
+      data: [
+        {
+          forum: forum.id,
+          name: 'general',
+        },
+        {
+          forum: forum.id,
+          name: 'coding',
+        },
+        {
+          forum: forum.id,
+          name: 'mapping',
+        },
+        {
+          forum: forum.id,
+          name: 'modeling',
+        },
+        {
+          forum: forum.id,
+          name: 'help & support',
+        },
+        {
+          forum: forum.id,
+          name: 'suggestions & feedback',
+        },
+      ],
+    });
+  });
+
+  // users?.forEach((user) => {
+  //   updateUsersTopics(topics, user);
+  // });
 }
 
 main()
